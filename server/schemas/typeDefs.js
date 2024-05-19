91d83fea639af8ef266124eacda59b6519367d6e
MauryIV <mauryhughesiv@gmail.com>
2024-05-18 20:56:56 -0600

    Completed the schemas index.js and typeDefs(adding all listed in assignment readme.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+const typeDefs =`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me(userId: ID!): User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, title: String, bookId: ID, image: string,link: String): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports=typeDefs
