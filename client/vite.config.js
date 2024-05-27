ca5f75706416c9579336639e085068e371346809
MauryIV <mauryhughesiv@gmail.com>
2024-05-27 15:56:07 -0600

    Troubleshooting deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
     port: 4001,
     open: true,
     proxy: {
