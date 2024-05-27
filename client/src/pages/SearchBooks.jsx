c552d2e0268122ddcd024cd69f6da6204ae33e0d
MauryIV <mauryhughesiv@gmail.com>
2024-05-27 13:24:44 -0600

    Slight grammer change for personal preference.

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -147,7 +147,7 @@ const SearchBooks = () => {
                         {savedBookIds?.some(
                           (savedBookId) => savedBookId === book.bookId
                         )
-                          ? "This book has already been saved!"
+                          ? "This book has been saved!"
                           : "Save this Book!"}
                       </Button>
                     )}
