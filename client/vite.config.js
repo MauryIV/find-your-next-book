22586872a5d357063bb2cb9490ba2cff118b1843
MauryIV <mauryhughesiv@gmail.com>
2024-05-27 14:52:21 -0600

    Troubleshooting deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
     port: 3000,
     open: true,
     proxy: {
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = 4001;
+const PORT = process.env.PORT || 4001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
