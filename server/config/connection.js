6e254f6c5a0a34532a07099d3d513e956ac4babc
MauryIV <mauryhughesiv@gmail.com>
2024-05-29 22:02:50 -0600

    Deployment troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:0/googlebooks');
 
 module.exports = mongoose.connection;
