95b855fffa6c7a35236a4732af43f703e5a98a27
MauryIV <mauryhughesiv@gmail.com>
2024-05-30 10:41:06 -0600

    Deployment troubleshooting

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ const startApolloServer = async () => {
     });
   }
 
+  db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
