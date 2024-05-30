2fa5bdc4fb0cae1bfce44b6676bf45b428695311
MauryIV <mauryhughesiv@gmail.com>
2024-05-30 10:47:57 -0600

    Deployment troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/googlebooks';
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Connection error:', err);
+})
 
 module.exports = mongoose.connection;
