f2d0fb39ca61d993e4041feba78638910916dccf
MauryIV <mauryhughesiv@gmail.com>
2024-05-30 11:02:47 -0600

    Deployment troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,8 +6,6 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/googlebooks
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
